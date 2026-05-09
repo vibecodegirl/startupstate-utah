a20f657d04a6825ca9398011d4f020c47a301549
Deborah Thackinh <deborah@cleverplan.co>
2026-05-09 02:34:02 +0000

    File changes

--- src/pages/StartupMap.jsx
+++ src/pages/StartupMap.jsx
@@ -223,7 +223,7 @@ export default function StartupMap() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   />
-                  {mappable.map(s => (
+                  {(selected ? mappable.filter(s => s.id === selected.id) : mappable).map(s => (
                     <Marker
                       key={s.id}
                       position={[s.latitude, s.longitude]}
